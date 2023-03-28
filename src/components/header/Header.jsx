54afbc6887fb46f5480e31e2c45500c6a5488b65
Hasib24 <hasibislam1488@gmail.com>
2023-03-28 13:55:34 +0600

    add header with nav

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+import Logo  from './../../images/Logo.svg';
+import React from 'react';
+
+const Header = () => {
+    console.log(3)
+    return (
+        <div className='header'>
+            <div className="header_logo"><img src={Logo} alt="" /></div>
+            <div className="header_nav">
+                <a href="/abc">Order</a>
+                <a href="/abc">Order Review</a>
+                <a href="/abc">Manage Inventory</a>
+                <a href="/abc">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
