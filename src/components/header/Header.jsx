caabcc3e90cb04de1cb9d07a26c3065491477166
Hasib24 <hasibislam1488@gmail.com>
2023-03-29 14:07:33 +0600

    initialize tailwind css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,21 @@
-import './Header.css'
+
 import Logo  from './../../images/Logo.svg';
 import React from 'react';
 
 const Header = () => {
     console.log(3)
     return (
-        <div className='header'>
-            <div className="header_logo"><img src={Logo} alt="" /></div>
-            <div className="header_nav">
-                <a href="/abc">Order</a>
-                <a href="/abc">Order Review</a>
-                <a href="/abc">Manage Inventory</a>
-                <a href="/abc">Login</a>
+        <div className='bg-slate-900 grid md:grid-cols-3 justify-between px-10 py-5'>
+
+            <div className=""><img className='mx-auto'  src={Logo} alt="" /></div>
+
+            <div></div>
+
+            <div className="text-white grid grid-flow-col items-center">
+                <a className='' href="/abc">Order</a>
+                <a className='' href="/abc">Order Review</a>
+                <a className='' href="/abc">Manage Inventory</a>
+                <a className='' href="/abc">Login</a>
             </div>
         </div>
     );
